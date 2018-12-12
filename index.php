ef36673c1c78dcf1b4abe4011735510d4f6304d1
Fa <ngomfa@gmail.com>
2018-12-12 11:08:28 +0100

    exo5

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Partie9_exo5</title>
+</head>
+<body>
+  <h1>Exo5:</h1>
+  <p>Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016</p>
+  <div class="container" id="date">
+    <?php
+    $datetime1 = new DateTime('2016-05-16'); //le mot clé new sert à instancier la classe Date Time qui fait de $datetime1 un objet.
+    $datetime2 = new DateTime('2018-11-30');
+    $interval = $datetime1->diff($datetime2);//diff retourne la différence interval entre deux objets.
+    echo 'Le nombre de jours qui nous sépare est de' . ' ' . $interval->format('%a');// retourne
+
+    ?>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
